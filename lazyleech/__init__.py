3335bbc53950e261a8827d23842c891ea09f90d0
NotDream72 <85627060+NotDream72@users.noreply.github.com>
2021-06-26 14:55:33 +0530

    Update __init__.py

--- lazyleech/__init__.py
+++ lazyleech/__init__.py
@@ -4,6 +4,9 @@ import aiohttp
 from io import BytesIO, StringIO
 from pyrogram import Client
 
+users ={}
+user_time = {}
+
 API_ID = os.environ.get('API_ID')
 API_HASH = os.environ.get('API_HASH')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -14,14 +17,14 @@ EVERYONE_CHATS = os.environ.get('EVERYONE_CHATS')
 EVERYONE_CHATS = list(map(int, EVERYONE_CHATS.split(' '))) if EVERYONE_CHATS else [-1001378211961]
 ADMIN_CHATS = os.environ.get('ADMIN_CHATS')
 ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else [441422215]
-ALL_CHATS = EVERYONE_CHATS + ADMIN_CHATS
+ALL_CHATS = ADMIN_CHATS
 
 PROGRESS_UPDATE_DELAY = int(os.environ.get('PROGRESS_UPDATE_DELAY', 5))
 MAGNET_TIMEOUT = int(os.environ.get('LEECH_TIMEOUT', 60))
 LEECH_TIMEOUT = int(os.environ.get('LEECH_TIMEOUT', 300))
 
 logging.basicConfig(level=logging.INFO)
-app = Client('lazyleech', API_ID, API_HASH, plugins={'root': os.path.join(__package__, 'plugins')}, bot_token=BOT_TOKEN, test_mode=TESTMODE, parse_mode='html', sleep_threshold=30)
+app = Client('lazyleech', API_ID, API_HASH, plugins={'root': os.path.join(__package__, 'plugins')}, bot_token=BOT_TOKEN, test_mode=TESTMODE, parse_mode='html', sleep_threshold=30, workers=100)
 session = aiohttp.ClientSession()
 help_dict = dict()
 preserved_logs = []
